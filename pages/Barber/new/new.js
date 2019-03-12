8244b3f942252a27f6f720c9867709db4f8eb769
AntiWeidongGit <1721512146@qq.com>
2019-03-12 18:59:36 +0800

    new
    new...

--- pages/Barber/new/new.js
+++ pages/Barber/new/new.js
@@ -2,8 +2,8 @@
 
 var app = getApp()
 var fileData = require('../../../utils/data.js')
-var util = require('../../../utils/util')
-
+var util = require('../../../utils/util');
+var api = require('../../../config/api.js');
 Page({
 
   /**
@@ -30,20 +30,124 @@ Page({
     winWidth: 0,
     winHeight: 0,
     // tab切换
-    currentTab: 0
+    currentTab: 0,
+    barberList: [],
+    barberName: '',
+    barberId: ''
 
   },
+  toServerItem: function (e) {
+    var that = this;
+    var index = e.target.id;
+    console.log("barberId:::" + that.data.barberList[index - 1].barberId);
+    that.setData({
+      barberId: that.data.barberList[index - 1].barberId
+    })
+    that.getBarberInfo();
+  },
+
+  toServer: function () {
+    var that = this;
+    console.log("点击预约后-barberName:--" + that.data.barberName + "barberId:--" + that.data.barberId);
+    wx.navigateTo({
+      url: '../../Subcribe/Subscribe_action/ServerItem?barberId=' + that.data.barberId + '&barberName=' + that.data.barberName
+    })
+  },
+
+  getBarberList: function () {
+    var that = this;
+    var bizContent = {
+    }
+    util.weshowRequest(
+      api.BarberList,
+      bizContent,
+      'POST').then(res => {
+        var a = JSON.stringify(res.data);
+        var barberinfo = JSON.stringify(res.data.bizContent.list);
+        that.setData({
+          barberList: res.data.bizContent.list,
+        });
+        util.stopRefreshing;
+        util.waitUpdate;
+      }).catch((err) => {
+        console.log('barberlist err :' + err);
+        util.stopRefreshing;
+        // wx.showToast({
+        //   title: '正在获取数据…',
+        //   icon: 'loading',
+        //   duration: 3000,
+        //   mask: true
+        // });
+      });
+  },
+
+  getBarberInfo: function () {
+    var that = this;
+    var bizContent = {
+      "barberId": that.data.barberId
+    }
+    console.log(bizContent);
+    util.weshowRequest(
+      api.Getbarberinfo,
+      bizContent,
+      'POST').then(res => {
+        that.setData({
+          barberName: res.data.bizContent.barberinfo.name
+        })
+        console.log("request-barberName:" + this.data.barberName);
+        this.toServer();
+      }).catch((err) => {
+        console.log('barberlist err :' + err);
+        util.stopRefreshing;
+      });
+  },
+
   bindCasPickerChange: function (e) {
     console.log('Category picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       casIndex: e.detail.value
     })
   },
+  bookTap: function () {
+    // console.log('getStoreList ' + api.StoreList);
+    //wx.showNavigationBarLoading();
+    var that = this;
+    var bizContent = {
+      "barberId": that.data.barberId
+    }
+    console.log(bizContent);
+    util.weshowRequest(
+      api.Getbarberinfo,
+      bizContent,
+      'POST').then(res => {
+
+        that.setData({
+          barberName: res.data.bizContent.barberinfo.name
+        })
+
+        console.log("request-barberName:" + this.data.barberName);
+        this.toServer();
+        // util.stopRefreshing;
+        // util.waitUpdate;
+      }).catch((err) => {
+        console.log('barberlist err :' + err);
+        // fail
+        util.stopRefreshing;
+        // wx.showToast({
+        //   title: '正在获取数据…',
+        //   icon: 'loading',
+        //   duration: 3000,
+        //   mask: true
+        // });
+      });
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
     var that = this
+    this.getBarberList();
     that.setData({
       list: that.data.navSectionItems,
       orderlist: that.data.orderItems
