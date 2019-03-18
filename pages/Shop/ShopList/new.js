b28b93b04b5370c397840b42fac36f3a64eee29d
weshowapp <31146098+weshowapp@users.noreply.github.com>
2019-03-18 22:02:34 +0800

    Revert "店铺列表新修改"
    This reverts commit 92ffd8e278ebc5de5f88c8567d0508e85b8e4ff9.

--- pages/Shop/ShopList/new.js
+++ pages/Shop/ShopList/new.js
@@ -24,21 +24,21 @@ Page({
       "id": "21",
       "text": "离我最近"
     },
-    {
-      "id": "30",
-      "text": "人气最高"
-    }, {
-      "id": "22",
-      "text": "面积最大"
-    }
+      {
+        "id": "30",
+        "text": "人气最高"
+      }, {
+        "id": "22",
+        "text": "面积最大"
+      }
     ],
     selectArray1: [{
       "id": "10",
       "text": "只看自营店"
     }, {
-      "id": "21",
-      "text": "只看社区店"
-    }],
+        "id": "21",
+        "text": "只看社区店"
+      }],
     banner_url: fileData.getBannerData(),
     indicatorDots: true,
     vertical: false,
